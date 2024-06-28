2508824557a40c1038696a44672409f39129031a
Finland93 <122778643+Finland93@users.noreply.github.com>
2024-06-28 21:07:33 +0300

    Update single.php

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <?php if (has_post_thumbnail()) : ?>
         <div class="hero-section mb-5">
-            <img src="<?php the_post_thumbnail_url('large'); ?>" class="img-fluid w-100" alt="<?php the_title_attribute(); ?>">
+            <img src="<?php the_post_thumbnail_url('hero-image'); ?>" class="img-fluid w-100" alt="<?php the_title_attribute(); ?>">
         </div>
     <?php endif; ?>
 <?php endwhile; endif; ?>
