7e9b9c035e354d1e61b634809b08a2d5578a58e3
Finland93 <122778643+Finland93@users.noreply.github.com>
2024-08-29 10:16:09 +0300

    Update page.php

--- page.php
+++ page.php
@@ -8,11 +8,12 @@
     <?php endif; ?>
 <?php endwhile; endif; ?>
 
-<main class="container mt-5">
+<main class="container mt-5" itemscope itemtype="https://schema.org/WebPage">
     <div class="row">
         <div class="col-md-12">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <div <?php post_class(); ?>>
+                <div <?php post_class(); ?> itemprop="mainEntity" itemscope itemtype="https://schema.org/CreativeWork">
+                    <meta itemprop="name" content="<?php the_title_attribute(); ?>">
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
