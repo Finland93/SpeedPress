17e56d9637c4ea89ad846e4b76fce8ec43c72c2b
Finland93 <122778643+Finland93@users.noreply.github.com>
2024-08-29 09:30:23 +0300

    Update single.php

--- single.php
+++ single.php
@@ -2,17 +2,29 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <?php if (has_post_thumbnail()) : ?>
-        <div class="hero-section mb-5">
-            <img src="<?php the_post_thumbnail_url('hero-image'); ?>" class="img-fluid w-100" alt="<?php the_title_attribute(); ?>">
+        <div class="hero-section mb-5" itemscope itemtype="http://schema.org/ImageObject">
+            <img src="<?php the_post_thumbnail_url('hero-image'); ?>" class="img-fluid w-100" alt="<?php the_title_attribute(); ?>" itemprop="url">
+            <meta itemprop="width" content="1200"> <!-- Add the correct width of your image -->
+            <meta itemprop="height" content="675"> <!-- Add the correct height of your image -->
         </div>
     <?php endif; ?>
 <?php endwhile; endif; ?>
 
-<main class="container mt-5">
+<main class="container mt-5" itemscope itemtype="http://schema.org/Article">
+    <link itemprop="url" href="<?php echo esc_url(get_permalink()); ?>">
     <div class="row">
         <div class="col-md-8">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <article <?php post_class(); ?>>
+                <article <?php post_class(); ?> itemprop="articleBody">
+                    <header>
+                        <h1 itemprop="headline"><?php the_title(); ?></h1>
+                        <div class="entry-meta">
+                            <time itemprop="datePublished" datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
+                            <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+                                by <span itemprop="name"><?php the_author(); ?></span>
+                            </span>
+                        </div>
+                    </header>
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
